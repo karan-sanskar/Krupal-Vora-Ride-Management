3087e21ea190848cc8071c41a7f70bb36f72d112
krupalvora <krupal.vora@sakec.ac.in>
2024-06-20 17:17:13 +0000

    Added Total Amount Logic and handeled exceptions

--- ride_managment/ride_managment/doctype/ride_booking/ride_booking.py
+++ ride_managment/ride_managment/doctype/ride_booking/ride_booking.py
@@ -6,9 +6,16 @@ from frappe.model.document import Document
 import frappe
 class RideBooking(Document):
 	def after_insert(self):
-		l=frappe.get_all("Ride Add On", filters = dict(parent=self.name),fields = ['amount'])
 		amt=0
-		for i in l:
-			amt=amt+int(i['amount'])
-		self.total_amount=(self.price_per_km*self.estimated_km)+amt
-		frappe.log_error(amt)
+		try:
+			l=frappe.get_all("Ride Add On", filters = dict(parent=self.name),fields = ['amount'])
+			for i in l:
+				amt=amt+int(i['amount'])
+		except:
+			frappe.log_error("No records in child table")
+		try:
+			self.total_amount=(self.price_per_km*self.estimated_km)+amt
+		except:
+			frappe.log_error("Estimated KM not entered")
+		
+		
