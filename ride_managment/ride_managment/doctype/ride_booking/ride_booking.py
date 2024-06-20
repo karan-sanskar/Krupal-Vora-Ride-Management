7d447c18c7ddbf15421c08618061de47b2f18e9b
krupalvora <krupal.vora@sakec.ac.in>
2024-06-20 17:13:01 +0000

    Added Total Amount Logic

--- ride_managment/ride_managment/doctype/ride_booking/ride_booking.py
+++ ride_managment/ride_managment/doctype/ride_booking/ride_booking.py
@@ -3,6 +3,12 @@
 
 # import frappe
 from frappe.model.document import Document
-
+import frappe
 class RideBooking(Document):
-	pass
+	def after_insert(self):
+		l=frappe.get_all("Ride Add On", filters = dict(parent=self.name),fields = ['amount'])
+		amt=0
+		for i in l:
+			amt=amt+int(i['amount'])
+		self.total_amount=(self.price_per_km*self.estimated_km)+amt
+		frappe.log_error(amt)
