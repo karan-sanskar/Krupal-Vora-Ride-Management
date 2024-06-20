0382b23de9e37866c3d294a95ecda3f25a85eaa6
krupalvora <krupal.vora@sakec.ac.in>
2024-06-20 07:47:13 +0000

    Added Doctypes

---
+++ ride_managment/ride_managment/doctype/ride_customer/ride_customer.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, krupal vora and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Ride Customer', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ ride_managment/ride_managment/doctype/vehicle_ride/vehicle_ride.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, krupal vora and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class VehicleRide(Document):
+	pass
